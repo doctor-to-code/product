d7a66d0ce4c81418daf3b606ea34c8dd97dd47cb
Peter Yang <fgdeeerry@gmail.com>
2021-01-03 22:06:31 +0800

    writing mode of text

--- products.py
+++ products.py
@@ -11,3 +11,7 @@ print(products)
 
 for p in products:
 	print(p[2]) #p[2]就是指date，當我們想要提取清單中特定的欄位時可以使用的方式
+
+with open('products.csv', 'w') as f: 
+	for p in products:
+		f.write(p[0]+ ',' + p[1]+ ',' + p[2]+ ',' + p[3]+ '\n')
