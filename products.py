9893c4a0fea4b2e72dbc65a2bc9335c00c6f6ed0
Peter Yang <fgdeeerry@gmail.com>
2021-01-04 09:52:56 +0800

    excel formation

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入價格')
+	price = int(price)
 	date = input('請輸入購買日期')
 	location = input('請輸入購買地點')
 	products.append([name, price, date, location])
@@ -12,6 +13,7 @@ print(products)
 for p in products:
 	print(p[2]) #p[2]就是指date，當我們想要提取清單中特定的欄位時可以使用的方式
 
-with open('products.csv', 'w') as f: 
+with open('products.csv', 'w', encoding'utf-8') as f: 
+	f.write('商品,價格,購買日期,購買地點\n')
 	for p in products:
-		f.write(p[0]+ ',' + p[1]+ ',' + p[2]+ ',' + p[3]+ '\n')
+		f.write(p[0]+ ',' + str(p[1])+ ',' + p[2]+ ',' + p[3]+ '\n')
