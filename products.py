9ef99f6b9431f0ffdfebd0aa3160c6e1288e9cb5
Peter Yang <fgdeeerry@gmail.com>
2021-01-05 10:21:17 +0800

    integration commit

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
-products =[]
+#讀取檔案
+products = []
+with open('products.csv', 'r', encoding='Big5') as f:
+	for line in f:
+		if '商品,價格,購買日期,購買地點' in line:
+			continue #繼續(continue和break不同之處在於continue只是跳過該階段而非離開迴圈loop)
+		name, price, date, location = line.strip().split(',')
+		products.append([name, price, date, location])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('請輸入商品明稱')
 	if name == 'q':
@@ -13,7 +23,8 @@ print(products)
 for p in products:
 	print(p[2]) #p[2]就是指date，當我們想要提取清單中特定的欄位時可以使用的方式
 
-with open('products.csv', 'w', encoding'utf-8') as f: 
+#寫入檔案
+with open('products.csv', 'w', encoding='Big5') as f: 
 	f.write('商品,價格,購買日期,購買地點\n')
 	for p in products:
 		f.write(p[0]+ ',' + str(p[1])+ ',' + p[2]+ ',' + p[3]+ '\n')
