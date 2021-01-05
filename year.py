2fd1e50e11d90c08bbffb3f391bf393b84b8ce82
Peter Yang <fgdeeerry@gmail.com>
2021-01-05 11:39:50 +0800

    year

---
+++ year.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+	if year % 4 != 0:
+		return False
+	elif year % 100 != 0:
+		return True
+	elif year % 400 != 0:
+		return False
+	else:
+		return True
+
+y = is_leap(2014)
+if y == False:
+	print('平年')
+else:
+	print('閏年')
